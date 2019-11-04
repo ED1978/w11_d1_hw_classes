8bca1a79bb15b912e41509bf983d1aac9bc621ee
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-04 16:31:43 +0000

    refactor calculator tests to use doubles

--- classes_homework/src/test/java/CalculatorTest.java
+++ classes_homework/src/test/java/CalculatorTest.java
@@ -15,22 +15,22 @@ public class CalculatorTest {
 
     @Test
     public void canAdd() {
-        assertEquals(4.0, calculator.add(2, 2));
+        assertEquals(4.44, calculator.add(2.22, 2.22), 0.01);
     }
 
     @Test
     public void canSubtract() {
-        assertEquals(5.0, calculator.subtract(10, 5));
+        assertEquals(5.03, calculator.subtract(10.55, 5.52), 0.01);
     }
 
     @Test
     public void canMultiply() {
-        assertEquals(10.0, calculator.multiply(5, 2));
+        assertEquals(12.90, calculator.multiply(5.56, 2.32), 0.01);
     }
 
     @Test
     public void canDivide() {
-        assertEquals(10.0, calculator.divide(20, 2));
+        assertEquals(8.04, calculator.divide(20.67, 2.57), 0.01);
     }
 
 }
