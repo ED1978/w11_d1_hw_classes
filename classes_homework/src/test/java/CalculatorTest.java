1b6029228dd9834975926c556395b2fbc4164502
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-04 15:40:15 +0000

    calculator can add

---
+++ classes_homework/src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(4.0, calculator.add(2, 2));
+    }
+
+}
