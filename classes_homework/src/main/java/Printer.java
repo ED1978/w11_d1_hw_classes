ea2718e22324388cb97bb61f277a7baa0669fb67
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-04 16:41:54 +0000

    printer has sheets

---
+++ classes_homework/src/main/java/Printer.java
@@ -0,0 +1,13 @@
+public class Printer {
+
+    private int paperSheets;
+
+    public Printer(int paperSheets) {
+        this.paperSheets = paperSheets;
+    }
+
+    public int getSheets() {
+        return this.paperSheets;
+    }
+
+}
